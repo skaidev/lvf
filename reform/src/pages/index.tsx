d8275a646f964a8404f3a0952d19fe19ea16c76d
stormzy22 <aawweb23@gmail.com>
2021-06-10 11:29:15 +0100

    continue

--- reform/src/pages/index.tsx
+++ reform/src/pages/index.tsx
@@ -6,7 +6,28 @@ const index = (): JSX.Element => {
     <FrontLayout>
       <main>
         <section className="first">
-          <div className="first-cover"></div>
+          <div className="first-cover">
+            <div className="first-content d-flex align-items-center">
+              <div className="container text-white">
+                <h1 className="rochester p-0 mb-3 fa-5x">Love Reform</h1>
+                <p className="p-0 fa-3x mb-3">
+                  <b className="fw-bolder">Empowerment</b> &nbsp;
+                  <span>
+                    for Startups
+                    <br />
+                    Awarding &nbsp;
+                  </span>
+                  <b>Scholarships</b>
+                </p>
+                <p className="p-0">
+                  Economic empowerment is central to women’s ability to overcome
+                  poverty, cope with shocks and improve their well-being. When
+                  women realize their economic goals, whether it’s growing a
+                  business
+                </p>
+              </div>
+            </div>
+          </div>
         </section>
       </main>
     </FrontLayout>
