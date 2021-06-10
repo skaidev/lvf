ce85de81beb4eaf1c6f74a72a1927afaa137bdba
stormzy22 <aawweb23@gmail.com>
2021-06-10 08:41:18 +0100

    remaining the footer

--- reform/src/components/props/Props.tsx
+++ reform/src/components/props/Props.tsx
@@ -1,3 +1,3 @@
 export interface Props {
-  props: string | number;
+  props: string;
 }
--- reform/src/pages/index.tsx
+++ reform/src/pages/index.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Donating from "../components/icon/Donating";
+import Donations from "../components/icon/Donations";
 import Education from "../components/icon/Education";
 import Environment from "../components/icon/Environment";
+import Gvolunteer from "../components/icon/Gvolunteer";
 import Health from "../components/icon/Health";
 import Join from "../components/icon/Join";
+import Members from "../components/icon/Members";
 import Volunteer from "../components/icon/Volunteer";
 import FrontLayout from "../layout/FrontLayout";
 
@@ -280,6 +283,213 @@ const index = (): JSX.Element => {
             </form>
           </div>
         </section>
+        <section className="eighth text-white py-5">
+          <div className="container">
+            <p className="text-center fa-2x mb-3">Testimonials</p>
+            <h2 className="text-center fw-bolder">
+              What&nbsp;
+              <mark className="bg-warning mb-5 p-0 text-white">People</mark> Say
+              About Us
+            </h2>
+            <div className="loader mb-4 row row-cols-1 justify-content-center justify-content-md-around row-cols-sm-1 row-cols-md-3">
+              <div className="col mb-2  two">
+                <span>
+                  <img src="Ellipse 8.png" alt="" />
+                </span>
+                <span></span>
+              </div>
+              <div className="col mb-2  two">
+                <span>
+                  <img src="Ellipse 9.png" alt="" />
+                </span>
+                <span></span>
+              </div>
+              <div className="col mb-2 two">
+                <span>
+                  <img src="Ellipse 10.png" alt="" />
+                </span>
+                <span></span>
+              </div>
+            </div>
+            <p className="text-center mb-4 fa-2x ">Charles Cliff</p>
+            <p className="text-center fw-lighter fa-2x mb-5">
+              “I’m so very grateful to the Love Vision Foundation <br /> charity
+              – and for the great and important work <br /> that they do for
+              other people.”
+            </p>
+            <div className="row blur py-5 row-cols-1 justify-content-center justify-content-sm-between row-cols-sm-2 row-cols-md-3 g-3">
+              {blurItem.map((item) => (
+                <div className="col " key={item.id}>
+                  <div
+                    className="card border-0 bg-transparent d-flex flex-row align-items-center h-100"
+                    style={{ gap: "1rem" }}
+                  >
+                    {item.icon}
+                    <div className="txt">
+                      <h2 className="fw-bold p-0 m-0 mb-1">{item.num}</h2>
+                      <h6>{item.txt}</h6>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
+        <section className="ninth py-5">
+          <div className="container">
+            <h2 className="text-center mb-4 fw-bolder">Our Amaizing Team</h2>
+            <p className="text-center mb-4">
+              We cant do it alone – from crisis to sustainably. There is no
+              limit to what you can do, from a <br /> bake sale to a 10k run
+              your efforts will local person in crisis much more than food, but
+              hope for <br /> the future.
+            </p>
+            <div className="row mb-4 row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+              {teamCard.map((card) => (
+                <div className="col team" key={card.id}>
+                  <div className="card team-card h-100">
+                    <img src={`team-${card.id}.png`} alt="" />
+                    <aside
+                      className="d-flex align-items-center text-white justify-content-center"
+                      style={{ gap: "1rem" }}
+                    >
+                      <i className="fab fa-2x fa-facebook"></i>
+                      <i className="fab fa-2x fa-twitter"></i>
+                      <i className="fab fa-2x fa-instagram"></i>
+                    </aside>
+                    <div
+                      className="text-center py-4 m-0 team-card-abs  text-white bg-warning w-100"
+                      style={{ zIndex: 1 }}
+                    >
+                      <h3 className="fw-bold mb-2"> {card.title} </h3>
+                      <h5>{card.body}</h5>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className="text-center see-all d-flex">
+              <hr />
+              <span className="text-primary fw-bold">See All</span>
+              <hr />
+            </div>
+          </div>
+        </section>
+        <section className="tenth py-5">
+          <div className="container">
+            <h2 className="text-center text-warning mb-4">
+              Our Successful Cases
+            </h2>
+            <h1 className="text-center fw-bolder mb-4 fa-2x">Gallery</h1>
+            <div className="row row-cols-1 mb-4 row-cols-sm-2 row-cols-md-3 g-3">
+              {galley.map((card) => (
+                <div className="col" key={card.id}>
+                  <div className="card border-0 bg-transparent">
+                    <img src={`g${card.id}.png`} alt="" className="card-img" />
+                    <div className="card-body bg-white p-2">
+                      <h5 className=" p-0 mb-4 fw-bold">
+                        Cancer support - For less priviledged children
+                      </h5>
+                      <div
+                        className="row-1 text-muted mb-2 d-flex align-items-center"
+                        style={{ gap: "1rem" }}
+                      >
+                        <i className="fas fa-calendar-alt"></i>
+                        <span> {card.date} </span>
+                      </div>
+                      <div
+                        className="row-1 text-muted d-flex align-items-center"
+                        style={{ gap: "1rem" }}
+                      >
+                        <i className="fas fa-map-marker-alt"></i>
+                        <span> {card.location} </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className="text-center see-all d-flex">
+              <hr />
+              <span className=" fw-bold">See All</span>
+              <hr />
+            </div>
+          </div>
+        </section>
+        <section className="eleven py-5">
+          <div className="container">
+            <h1 className="text-center text-secondary mb-3">Contact Us</h1>
+            <p className="text-muted text-center mb-5">
+              We cant do it alone – from crisis to sustainably. There is no
+              limit to what you can do, from a bake sale to a 10k run your help
+              give food, but hope for the future.
+            </p>
+            <div className="contact-icon d-flex mb-5 flex-column flex-sm-row justify-content-around">
+              <div className="mail d-flex flex-column align-items-center">
+                <i className="fas fa-2x fa-map-marker-alt px-4 py-3 text-light mb-3 bg-warning rounded"></i>
+                <span className="text-center">jointheirs5@gmail.com</span>
+              </div>
+              <div className="address d-flex flex-column align-items-center">
+                <i className="fas fa-2x fa-envelope px-4 py-3 text-light mb-3 bg-warning rounded"></i>
+                <span className="text-center">
+                  No 6 Green Tower, Street Rumuodara
+                </span>
+              </div>
+              <div className="tel d-flex flex-column align-items-center">
+                <i className="fas fa-2x fa-tty px-4 py-3 text-light mb-3 bg-warning rounded"></i>
+                <span className="text-center">+234 9012624162</span>
+              </div>
+            </div>
+            <form >
+              <div
+                className="d-flex mb-4 flex-column flex-md-row"
+                style={{ gap: "2rem" }}
+              >
+                <input
+                  type="text"
+                  className="form-control py-3"
+                  required
+                  placeholder="Charles Cliff"
+                  spellCheck={true}
+                />
+                <input
+                  type="email"
+                  multiple
+                  className="form-control py-3"
+                  placeholder="example@gmail.com"
+                  required
+                />
+              </div>
+              <input
+                type="text"
+                className=" form-control py-3 mb-4"
+                placeholder="Subject"
+                spellCheck
+              />
+
+              <textarea
+                className="form-control form-control-lg mb-4"
+                id=""
+                cols={30}
+                rows={10}
+                spellCheck
+                placeholder="Type your Message"
+              ></textarea>
+              <button className="btn border-primary bg-primary text-white fw-bold px-4 py-2">
+                Send
+              </button>
+            </form>
+          </div>
+        </section>
+        <section className="map ">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3975.444085232075!2d7.032735314762972!3d4.86501199646747!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1069d27198c56a95%3A0x4bf2d3ebd3c1a2dd!2sJoint%20Heirs%20Assembly%20Int&#39;l%20Inc.!5e0!3m2!1sen!2sng!4v1621608305779!5m2!1sen!2sng"
+            height="450"
+            allowFullScreen={false}
+            loading="lazy"
+            style={{ width: "100%", border: 0, margin: 0, padding: 0 }}
+          ></iframe>
+        </section>
       </main>
     </FrontLayout>
   );
@@ -365,5 +575,38 @@ const sixthCard = [
     bg: "rgba(158, 64, 35, 0.38)",
     img: 81,
     polygon: "rgba(255, 255, 255, 0.39)",
+  },
+];
+
+const blurItem = [
+  { id: 1, icon: <Donations />, num: "24,000+", txt: "Donations Complete" },
+  { id: 2, icon: <Gvolunteer />, num: "24,000+", txt: "Total Volunteers" },
+  { id: 3, icon: <Members />, num: "88,706 ", txt: "Community Members" },
+];
+
+const teamCard = [
+  { id: 1, title: "Secretary", body: "Save Homeless people" },
+  { id: 2, title: "Secretary", body: "Save Homeless people" },
+  { id: 3, title: "Secretary", body: "Save Homeless people" },
+];
+
+const galley = [
+  {
+    id: 1,
+    title: "Cancer support - For less priviledged children",
+    date: "5th june 2020",
+    location: "No 6 Mission Road",
+  },
+  {
+    id: 2,
+    title: "Cancer support - For less priviledged children",
+    date: "5th june 2020",
+    location: "No 6 Mission Road",
+  },
+  {
+    id: 3,
+    title: "Cancer support - For less priviledged children",
+    date: "5th june 2020",
+    location: "No 6 Mission Road",
   },
 ];
