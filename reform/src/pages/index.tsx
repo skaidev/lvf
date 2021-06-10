64a633dbe57e451462b009edafb9859008718cd2
stormzy22 <aawweb23@gmail.com>
2021-06-10 10:57:47 +0100

    started

---
+++ reform/src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import FrontLayout from "../layout/FrontLayout";
+
+const index = (): JSX.Element => {
+  return (
+    <FrontLayout>
+      <main>
+        <section className="first">
+          <div className="first-cover"></div>
+        </section>
+      </main>
+    </FrontLayout>
+  );
+};
+
+export default index;
