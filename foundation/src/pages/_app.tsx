e88ea7e19a80f15089b5d61e24a377697c172717
itzemorai87@gmail.com <itzemorai87@gmail.com>
2021-11-30 16:58:39 +0100

    push

--- foundation/src/pages/_app.tsx
+++ foundation/src/pages/_app.tsx
@@ -1,28 +1,16 @@
 import React from "react";
 import { AppProps } from "next/app";
 import "../styles/index.scss";
-<<<<<<< HEAD
 import "../styles/styles.scss";
-=======
-import "styles/styles.scss";
->>>>>>> d07c1c454951458db4b8a8a9ac275811d073c247
 import "animate.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
-<<<<<<< HEAD
   return (
     <div>
       <Component {...pageProps} />
     </div>
   );
-=======
-	return (
-		<div>
-			<Component {...pageProps} />
-		</div>
-	);
->>>>>>> d07c1c454951458db4b8a8a9ac275811d073c247
 };
 
 export default MyApp;
